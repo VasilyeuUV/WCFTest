1580b9e2dcd621afb13ca9e61ab81c41d8af851c
Uladzislau Vasilyeu <i3jiad@outlook.com>
2021-02-22 17:02:24 +0300

    ServerWinForms: EndpointModel

---
+++ FirstWcfServiceLibrary/ServerOnWindowsForms/Models/EndpointModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ServiceModel.Channels;
+
+namespace ServerOnWindowsForms.Models
+{
+    /// <summary>
+    /// Модель конечной точки
+    /// </summary>
+    internal class EndpointModel
+    {
+        /// <summary>
+        /// Адрес, где ожидать входящие сообщения 
+        /// </summary>
+        public Uri Uri { get; internal set; }
+
+        /// <summary>
+        /// Указание привязки, как обмениваться сообщениями
+        /// </summary>
+        public Binding Binding { get; internal set; }
+
+        /// <summary>
+        /// Контракт
+        /// </summary>
+        public Type Contract { get; internal set; }
+
+
+    }
+}
--- FirstWcfServiceLibrary/WinFormClient/Forms/MainForm.Designer.cs
+++ FirstWcfServiceLibrary/WinFormClient/Forms/MainForm.Designer.cs
@@ -32,16 +32,17 @@ namespace WinFormClient
             this.btnSendMessage = new System.Windows.Forms.Button();
             this.tbMessage = new System.Windows.Forms.TextBox();
             this.tbMain = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSendMessage
             // 
             this.btnSendMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSendMessage.Location = new System.Drawing.Point(224, 207);
+            this.btnSendMessage.Location = new System.Drawing.Point(12, 207);
             this.btnSendMessage.Name = "btnSendMessage";
-            this.btnSendMessage.Size = new System.Drawing.Size(146, 34);
+            this.btnSendMessage.Size = new System.Drawing.Size(210, 34);
             this.btnSendMessage.TabIndex = 1;
-            this.btnSendMessage.Text = "Send Message";
+            this.btnSendMessage.Text = "Send Message to WinSevice";
             this.btnSendMessage.UseVisualStyleBackColor = true;
             this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
             // 
@@ -51,7 +52,7 @@ namespace WinFormClient
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbMessage.Location = new System.Drawing.Point(12, 179);
             this.tbMessage.Name = "tbMessage";
-            this.tbMessage.Size = new System.Drawing.Size(358, 22);
+            this.tbMessage.Size = new System.Drawing.Size(484, 22);
             this.tbMessage.TabIndex = 2;
             // 
             // tbMain
@@ -62,14 +63,24 @@ namespace WinFormClient
             this.tbMain.Multiline = true;
             this.tbMain.Name = "tbMain";
             this.tbMain.ReadOnly = true;
-            this.tbMain.Size = new System.Drawing.Size(358, 161);
+            this.tbMain.Size = new System.Drawing.Size(484, 161);
             this.tbMain.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(302, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Send Message to WinForm";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(382, 253);
+            this.ClientSize = new System.Drawing.Size(508, 253);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.tbMessage);
             this.Controls.Add(this.btnSendMessage);
             this.Controls.Add(this.tbMain);
@@ -84,6 +95,7 @@ namespace WinFormClient
         private System.Windows.Forms.Button btnSendMessage;
         private System.Windows.Forms.TextBox tbMessage;
         private System.Windows.Forms.TextBox tbMain;
+        private System.Windows.Forms.Button button1;
     }
 }
 
