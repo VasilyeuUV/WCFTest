ac2804063eae4df4b9397553a23e713fe3651c98
Uladzislau Vasilyeu <i3jiad@outlook.com>
2021-02-22 14:56:58 +0300

    Создан проект с общими интерфейсами

---
+++ FirstWcfServiceLibrary/GeneralInterfaces/Contracts/IContractable.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace GeneralInterfaces.Contracts
+{
+    /// <summary>
+    /// Базовый интерфейс для контрактов службы WCF
+    /// </summary>
+    [ServiceContract]
+    public interface IContractable
+    {
+    }
+}
---
+++ FirstWcfServiceLibrary/GeneralInterfaces/Contracts/IFirstContract.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace GeneralInterfaces.Contracts
+{
+
+    /// <summary>
+    /// Интерфейс первого контракта
+    /// </summary>
+    public interface IFirstContract : IContractable
+    {
+        [OperationContract]
+        string GetData(string value);
+    }
+}
